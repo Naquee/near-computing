2c94c10a17e1f73dfb01508df15e6c746da3fbc1
Naquee <naqueealam99@gmail.com>
2024-01-02 22:11:01 +0530

    add new

--- src/app/components/LandingCard.jsx
+++ src/app/components/LandingCard.jsx
@@ -20,56 +20,31 @@ const LandingCard = () => {
       <Box bg="#2d2a2b">
         <Image src={hsc} alt="carasole" objectFit="cover" />
       </Box>
-      <Card
-        w={["100%", "32%"]}
+      <Box
+        w={["100%", "30%"]}
         pos={["static", "absolute"]}
-        top={["0", "6rem"]}
-        left={["0","1%"]}
+        top={["0", "26rem"]}
+        left={["0","37%"]}
         color={["black", "white"]}
         fontSize={["16px", "20px"]}
-        background="rgba(0, 136, 255, 0.5)"
+        fontWeight="bold"
+        // background="rgba(252, 249, 249, 0.68)"
         p={["0", "2rem"]}
         mt={["1rem", "0rem"]}
         shadow="lg"
         borderRadius="2xl"
       >
-      <motion.div
-      animate={animation}
-      transition={{ ease: "easeOut", duration: 2 }}
-    >
-      <Center
-        width={{ base: "95%", md: "100%" }}
-        m="auto"
-        color={COLORS.white}
-        p={["1rem","0"]}
-      >
-        <Heading size={{ base: "md", md: "xl" }} letterSpacing="1px">
-        Our Service
-        </Heading>
-      </Center>
-      <Divider p={["0","1rem"]}/>
-    </motion.div>
-
         {/* <TopHeading color={COLORS.white} headings="Our Service" /> */}
-        <Text p={["1rem","0"]}>
-          Unlock the power of seamless productivity with our laptop rental
-          services. Whether you are a student in need of a temporary setup or a
-          professional on the go, our diverse range of high-performance laptops
-          ensures you have the right tool for every task. Experience the latest
-          technology without the commitment of ownership – rent a laptop
-          tailored to your specific requirements. Enjoy flexible terms, reliable
-          performance, and hassle-free delivery. Elevate your work or study
-          environment instantly. Rent a laptop today and embrace the convenience
-          of top-tier computing without the long-term commitment. Your ideal
-          laptop, your terms, at your fingertips.
+        <Text p={["1rem","0"]} fontSize="30px" fontFamily="Merriweather">
+        Welcome to Near Computing
         </Text>
-      </Card>
+      </Box>
 
      <Card
         // w={["100%", "25%"]}
         pos={["static", "absolute"]}
         top={["0", "35rem"]}
-        left={["0","68%"]}
+        left={["0","78%"]}
         color={["black", "white"]}
         ml={["0","2rem"]}
         mt={["1rem", "0rem"]}
--- src/app/components/RepairCard.jsx
+++ src/app/components/RepairCard.jsx
@@ -115,3 +115,4 @@ const RepairCard = () => {
 };
 
 export default RepairCard;
+ 
