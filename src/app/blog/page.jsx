49149062c4a56928d6817ee7577ede9ea060cee6
Naquee <100348982+Naquee@users.noreply.github.com>
2024-01-10 23:31:37 +0530

    add counter

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,30 +1,45 @@
-import { Box,Text } from "@chakra-ui/react";
+"use client";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
-import hsc1 from "../../assets/hsc1.jpg"
+import hsc1 from "../../assets/blogBg.jpg";
 import Image from "next/image";
-
-const Blog = ({children}) => {
-    return (
-        <>
-        <Box>
-            <Text>Blog</Text>
-             <Box
-      backgroundImage={`url(${hsc1})`}
-      backgroundSize="cover"
-      backgroundPosition="center"
-      backgroundRepeat="no-repeat"
-      height="500px"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      color="white"
-      fontSize="2xl"
-    >
-      {children}
-    </Box>
-            {/* <Image src={hsc1} alt="Near Computing"/> */}
-        </Box>
-        </>
-    )
-}
-export default Blog
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import Counter from "../components/Counter";
+import Navbar from "../components/Navbar";
+const Blog = () => {
+  const [text] = useTypewriter({
+    words: ["Please", "Wait", "a", "Blog"],
+    loop: {},
+    typeSpeed: 200,
+    delaySpeed: 40,
+  });
+  return (
+    <>
+      <Navbar />
+      <Box
+        zIndex="-100"
+        position="fixed"
+        width="100%"
+        height="100vh"
+        // backgroundAttachment="fixed"
+        // backgroundPosition="center"
+        // backgroundRepeat="no-repeat"
+        // backgroundSize="cover"
+        // border="1px solid red"
+        mt="7rem"
+      >
+        <Image
+          src={hsc1}
+          alt="Near Computing"
+          layout="fill"
+          objectFit="cover"
+        />
+      </Box>
+      {/* <Heading textAlign="center" color="white">
+        {text}
+      </Heading> */}
+      <Counter duration={4 * 24 * 60 * 60 * 1000} />
+    </>
+  );
+};
+export default Blog;
---
+++ src/app/components/Counter.jsx
@@ -0,0 +1,37 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
+const Counter = ({ duration }) => {
+  const [time, setTime] = useState(duration);
+
+  const getFormatatedTime = (milisecond) => {
+    let totalSeconds = parseInt(Math.floor(milisecond / 1000));
+    let totalminutes = parseInt(Math.floor(totalSeconds / 60));
+    let totalHours = parseInt(Math.floor(totalminutes / 60));
+    let days = parseInt(Math.floor(totalHours / 24));
+
+    let seconds = parseInt(totalSeconds % 60);
+    let minutes = parseInt(totalminutes % 60);
+    let hours = parseInt(totalHours % 24);
+
+    return `${days}: ${hours}: ${minutes}: ${seconds}`;
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(time - 1000);
+    }, 1000);
+  }, [time]);
+
+  return (
+    <Box width="50%" m="auto" color="white" boxSizing="border-box">
+    <br />
+      <Heading mt='10rem'>Blog Page is Coming Soon...</Heading>
+      <br />
+      <br />
+      <Heading ml='5rem'>{getFormatatedTime(time)}</Heading>
+    </Box>
+  );
+};
+
+export default Counter;
